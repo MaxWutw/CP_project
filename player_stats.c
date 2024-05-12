631b71e7e67b377eebb1eb095f799725444d0b61
shadowhunter2155 <gojunyu@gmail.com>
2024-05-12 10:46:33 +0800

    chore: npc status

--- player_stats.c
+++ player_stats.c
@@ -6,15 +6,15 @@ static int32_t player_inventory[MAX_INVENTORY_SIZE] = {0};
 Player init_player(){
 	player_status = 0;
 	memset(player_inventory,-1,sizeof(int32_t)*MAX_INVENTORY_SIZE);
-	Player player = {player_status,set_status,get_status,player_inventory,obtain_item,check_item,delete_item};
+	Player player = {player_status,set_player_status,get_player_status,player_inventory,obtain_item,check_item,delete_item};
 	return player;
 }
 
-int32_t set_status(int32_t val){
+int32_t set_player_status(int32_t val){
 	player_status = val;
 	return 0;
 }
-int32_t get_status(){
+int32_t get_player_status(){
 	return player_status;
 }
 
@@ -44,4 +44,26 @@ int32_t delete_item(int32_t id){
 		}
 	}
 	return 1;
+}
+
+Npc init_npc(){
+	Npc npc = {0, set_npc_status, add_npc_status, get_npc_status};
+	return npc;
+}
+
+
+int32_t set_npc_status(Npc *npc, int32_t val){
+	npc->status_val = val;
+	return 0;
+}
+
+
+int32_t add_npc_status(Npc *npc, int32_t val){
+	npc->status_val += val;
+	return 0;
+}
+
+
+int32_t get_npc_status(Npc *npc){
+	return npc->status_val;
 }
